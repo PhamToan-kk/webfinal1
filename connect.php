8152d3ba0e78b8cb4301ccf14af1dd9b525b1b15
Toàn <toanpvgch17585#fpt.edu.vn>
2019-08-24 16:03:01 +0700

    hiaa

--- connect.php
+++ connect.php
@@ -1,4 +1,4 @@
 <?php
 
-$conn = pg_connect("host=ec2-107-20-155-148.compute-1.amazonaws.com port=5432 dbname=d1cg0pi0dv5r10 user=gsuiybcdvwxcxu password=509a4e854e8a54f23aad6af7143ca7546d09dd443223e69e01d4294354d0e7c") or die("Connect Unsuccessful");
+$conn = pg_connect("host=ec2-107-20-155-148.compute-1.amazonaws.com port=5432 dbname=d1cg0pi0dv5r10 user=gsuiybcdvwxcxu password=509a4e854e8a54f23aad6af7143ca7546d09dd443223e69e01d4294354d0e7cf") or die("Connect Unsuccessful");
 ?>
