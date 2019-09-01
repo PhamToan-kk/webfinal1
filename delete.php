774b56a0e94e85a184c86e4022124fc12e21ff73
Toàn <toanpvgch17585#fpt.edu.vn>
2019-09-02 00:11:53 +0700

    a

--- delete.php
+++ delete.php
@@ -3,5 +3,5 @@ require("connect.php");
 $id = $_POST['productid'];
 $sql = "DELETE FROM product WHERE productid = '$id'";
 pg_query($conn,$sql); 
-header("Location: /managing.php");
+header("Location: /index.php");
 ?>
